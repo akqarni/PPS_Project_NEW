34e69d1197f4f982498400cdaa440656da086749
Effathi5404 <emueffatpucse@gmail.com>
2021-11-07 04:17:01 -0500

    added buy and sell

--- PPS_Project/src/PPS_Project/web/servlets/Buy_and_Sell_Servlet.java
+++ PPS_Project/src/PPS_Project/web/servlets/Buy_and_Sell_Servlet.java
@@ -28,6 +28,7 @@ public class Buy_and_Sell_Servlet extends HttpServlet {
 	private User_DAO userDAO;
 	private Transactions_DAO transactionDAO;
 	private PPS_Price_DAO PPS_PriceDAO;
+	private PPS_Price latestPPSprice;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -38,6 +39,7 @@ public class Buy_and_Sell_Servlet extends HttpServlet {
         userDAO = new User_DAO();
 		transactionDAO = new Transactions_DAO();
 		PPS_PriceDAO = new PPS_Price_DAO();
+		
     }
     
     /**
@@ -94,21 +96,125 @@ public class Buy_and_Sell_Servlet extends HttpServlet {
 		}
 		
 		try {
+			int pps_buy_amount = Integer.parseInt(request.getParameter("PPS-to-buy-amount"));
+			System.out.println("requested pps amount");
+			System.out.println(pps_buy_amount);
 			user = userDAO.selectUser(user.getUser_email());
 			User root = userDAO.selectUser("root");
+			double user_dollar_balance=user.getDollar_balance();
+			System.out.println("requested user dollar balance");
+			System.out.println(user_dollar_balance);
+			int user_pps_balance=user.getPPS_balance();
+			
 			
 			int root_PPS_balance = root.getPPS_balance();
+			double root_dollar_amount=root.getDollar_balance();
 			
-			int PPS_Amount = Integer.parseInt(request.getParameter("PPS-to-buy-amount"));
+			System.out.println("root pps amount");
+			System.out.println(root_PPS_balance);
 			
 			PPS_Price PPS_price = PPS_PriceDAO.getLastest_PPS_price();
 			
 			double currentPPS_price = PPS_price.getPrice();
+			System.out.println("current pps price");
+			System.out.println(currentPPS_price);
+			 
+			double required_dollar_amount= (double)pps_buy_amount * currentPPS_price;
+			System.out.println("required dollar to buy pps");
+			System.out.println(required_dollar_amount);
+			
+			
+			double updated_pps_price= 1/((1/currentPPS_price)-1);
+			System.out.println("updated pps price");
+			System.out.println(updated_pps_price);
+			
+			int updated_root_pps_balance=root_PPS_balance-pps_buy_amount;
+			System.out.println("root updated pps balance");
+			System.out.println(updated_root_pps_balance);
+			
+			double updated_root_dollar_balance=root_dollar_amount+required_dollar_amount;
+			System.out.println("root updated dollar balance");
+			System.out.println(updated_root_dollar_balance);
+			
+			
+			int updated_user_pps_balance=user_pps_balance +pps_buy_amount;
+			System.out.println("buyer updated pps balance");
+			System.out.println(updated_user_pps_balance);
+			
+			double updated_user_dollar_balance=user_dollar_balance-required_dollar_amount;
+			
+			System.out.println("buyer updated dollar balance");
+			System.out.println(updated_user_dollar_balance);
+			
+			User latestUpdatedrootinfo = userDAO.selectUser("root");
+			User latestUpdatedbuyerinfo = userDAO.selectUser(user.getUser_email());
+		
+			
+			latestPPSprice=new PPS_Price(updated_pps_price);
+			
+			
+			
+			if (root_PPS_balance>=pps_buy_amount)
+			{
+				if(user_dollar_balance>=required_dollar_amount) {
+					
+					//update pps price
+					boolean isupdatedppsprice=PPS_PriceDAO.update_PPS_price(latestPPSprice);
+					System.out.println("isupdatedppsprice "+ isupdatedppsprice);
+					
+					//update root info
+				
+				latestUpdatedrootinfo .setPPS_balance(updated_root_pps_balance);
+				latestUpdatedrootinfo .setDollar_balance(updated_root_dollar_balance);
+				boolean isrootPPSUpdated = userDAO.updateUserPPSBalance(latestUpdatedrootinfo);
+    			System.out.println("isrootPPSUpdated: "+ isrootPPSUpdated);
+    			boolean isrootdollarUpdated = userDAO.updateUserDollarBalance(latestUpdatedrootinfo);
+    			System.out.println("isrootdollarUpdated : "+isrootdollarUpdated );
+    			
+    			
+    			
+//update buyer info
+				latestUpdatedbuyerinfo .setPPS_balance(updated_user_pps_balance);
+				latestUpdatedbuyerinfo.setDollar_balance(updated_user_dollar_balance);
+				boolean isbuyerPPSUpdated = userDAO.updateUserPPSBalance(latestUpdatedbuyerinfo);
+    			System.out.println("isbuyerPPSUpdated: "+ isbuyerPPSUpdated);
+    			boolean isbuyerdollarUpdated = userDAO.updateUserDollarBalance(latestUpdatedbuyerinfo);
+    			System.out.println("isbuyerdollarUpdated : "+isbuyerdollarUpdated );
+    			
+    			
+    			//update transaction table
+    			
+    			Transactions newBuyTransaction = new Transactions ();
+    			 newBuyTransaction.setTransaction_date(LocalDate.now().toString());
+    			 newBuyTransaction.setTransaction_time(LocalTime.now().toString());
+    			 newBuyTransaction.setDollar_amount(required_dollar_amount);
+    			 newBuyTransaction.setPPS_amount(pps_buy_amount);
+    			 newBuyTransaction.setTransaction_name("buy");
+    			 newBuyTransaction.setTransaction_from_email("root");
+    			 newBuyTransaction.setTransaction_to_email(user.getUser_email());
+    			boolean isTransferTransactionInserted = transactionDAO.insertTransaction( newBuyTransaction);
+    			System.out.println("isTrnsaferTransactionInserted: "+ isTransferTransactionInserted);
+    			
+    			
+    			
+    			
+    			
+    			
+				}
+				
+			}
+			
+			
+			
+			
+			
+			
 			
 			// Calculate the the price of the pps amount 
 			//double priceOfPPSAmount = do the calucalation here;
 			
-		
+			request.setAttribute("message", "Successful.");
+			request.getRequestDispatcher("user-buyPage.jsp").forward(request, response);
 			
 			return;
 		} catch (Exception e) {
@@ -145,13 +251,102 @@ public class Buy_and_Sell_Servlet extends HttpServlet {
 			int root_PPS_balance = root.getPPS_balance();
 			
 			int PPS_Amount = Integer.parseInt(request.getParameter("PPS-to-sell-amount"));
+			int seller_pps_amount=user.getPPS_balance();
+			double seller_dollar_balance=user.getDollar_balance();
+			
+			double root_current_dollar_balance=root.getDollar_balance();
+			int root_current_pps_amount=root.getPPS_balance();
+			
 			
 			PPS_Price PPS_price = PPS_PriceDAO.getLastest_PPS_price();
 			
 			double currentPPS_price = PPS_price.getPrice();
 			
-			// Calculate the the price of the pps amount 
-			//double priceOfPPSAmount = do the calucalation here;///
+			
+			double required_dollar_balance=(double)PPS_Amount *currentPPS_price;
+			
+			
+			
+			
+			
+			double updated_pps_price=1/((1/currentPPS_price)+1);
+			int seller_updated_pps_amount=seller_pps_amount-PPS_Amount ;
+			double seller_updated_dollar_balance=seller_dollar_balance+required_dollar_balance;
+			
+			
+			
+			int root_updated_pps_amount=root_current_pps_amount+ PPS_Amount;
+			double root_updated_dollar_balance=root_current_dollar_balance-required_dollar_balance;
+			
+			
+			
+			
+			User latestUpdatedrootinfo = userDAO.selectUser("root");
+			User latestUpdatedsellerinfo = userDAO.selectUser(user.getUser_email());
+		
+			
+			latestPPSprice=new PPS_Price(updated_pps_price);
+			
+			
+			if(PPS_Amount<=seller_pps_amount)
+			{
+				if(root_current_dollar_balance>= required_dollar_balance)
+				{
+					
+					
+					//update pps price
+					boolean isupdatedppsprice=PPS_PriceDAO.update_PPS_price(latestPPSprice);
+					System.out.println("isupdatedppsprice "+ isupdatedppsprice);
+					
+					//update root info
+					
+					latestUpdatedrootinfo .setPPS_balance(root_updated_pps_amount);
+					latestUpdatedrootinfo .setDollar_balance(root_updated_dollar_balance);
+					boolean isrootPPSUpdated = userDAO.updateUserPPSBalance(latestUpdatedrootinfo);
+	    			System.out.println("isrootPPSUpdated: "+ isrootPPSUpdated);
+	    			boolean isrootdollarUpdated = userDAO.updateUserDollarBalance(latestUpdatedrootinfo);
+	    			System.out.println("isrootdollarUpdated : "+isrootdollarUpdated );
+					
+	    			
+	    			//update seller info
+	    			latestUpdatedsellerinfo .setPPS_balance(seller_updated_pps_amount);
+	    			latestUpdatedsellerinfo .setDollar_balance(seller_updated_dollar_balance);
+					boolean issellerPPSUpdated = userDAO.updateUserPPSBalance(latestUpdatedsellerinfo );
+	    			System.out.println("issellerPPSUpdated: "+ issellerPPSUpdated);
+	    			boolean issellerdollarUpdated = userDAO.updateUserDollarBalance(latestUpdatedsellerinfo );
+	    			System.out.println("issellerdollarUpdated : "+issellerdollarUpdated );
+	    			
+	    			
+	    			
+	    			
+	    			
+	    			
+	    			
+	    			//update transaction table
+	    			
+	    			Transactions newsellTransaction = new Transactions ();
+	    			 newsellTransaction.setTransaction_date(LocalDate.now().toString());
+	    			 newsellTransaction.setTransaction_time(LocalTime.now().toString());
+	    			 newsellTransaction.setDollar_amount(required_dollar_balance);
+	    			 newsellTransaction.setPPS_amount(PPS_Amount);
+	    			 newsellTransaction.setTransaction_name("sell");
+	    			 newsellTransaction.setTransaction_from_email(user.getUser_email());
+	    			 newsellTransaction.setTransaction_to_email("root");
+	    			boolean isTransferTransactionInserted = transactionDAO.insertTransaction(  newsellTransaction);
+	    			System.out.println("isTrnsaferTransactionInserted: "+ isTransferTransactionInserted);
+	    			
+	    			request.setAttribute("message", "Successful.");
+	    			request.getRequestDispatcher("user-sellPage.jsp").forward(request, response);
+					
+					
+					
+				}
+				
+				
+				
+				
+			}
+			
 			
 		
 			
